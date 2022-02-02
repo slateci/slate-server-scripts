69765b6ec308c3a9fb23d5d90bc1b582fd4f879a
Suchandra Thapa <suchandra@gmail.com>
2022-02-02 11:45:04 -1000

    Allow setting distro when building

--- rpm/build-rpm.sh
+++ rpm/build-rpm.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+set -x
 VERSION=`grep Version slate-server-scripts.spec | cut -f 2 -d: | tr -d [:space:]`
 TAR_DIR="slate-server-scripts-$VERSION"
 mkdir $TAR_DIR
 cp ../*.sh $TAR_DIR
 tar cvzf $TAR_DIR.tar.gz $TAR_DIR
-cp $TAR_DIR.tar.gz $RPM_BUILD_ROOT/SOURCES
-cp slate-server-scripts.spec $RPM_BUILD_ROOT/SRPMS
-mock --buildsrpm --spec slate-server-scripts.spec --sources $TAR_DIR.tar.gz --resultdir=.
-mock *.src.rpm  --resultdir=.
- 
+rm -fr $TAR_DIR
+EXTRA_OPTS=""
+if [[ ! -z "$1" ]];
+then
+  EXTRA_OPTS="-r $1"
+fi
+mock $EXTRA_OPTS --buildsrpm --spec slate-server-scripts.spec --sources $TAR_DIR.tar.gz --resultdir=.
+mock $EXTRA_OPTS *.src.rpm  --resultdir=. 
